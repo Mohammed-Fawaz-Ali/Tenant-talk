acb168ae0a497ef34c1132bc73f83daaaf334b11
Mohammed-Fawaz-Ali <mdfawazali18@gmail.com>
2026-03-23 15:39:16 +0530

    fix: correct mongodb uri and authentication

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load env variables
+dotenv.config();
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
@@ -7,15 +15,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Test Route
+// Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Port setup
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
